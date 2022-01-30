8a6e4a554408b46e1ad2c4f95fc792209f8ad4ae
Yama <yamawaki00@gmail.com>
2022-01-30 15:24:33 +0900

    modify: update package version

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('node-sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const ejs = require('gulp-ejs');
 const cleanCss = require('gulp-clean-css');
